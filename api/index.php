49ea6c1d5ab9e2b080bb5edbf9ca75f8104a4955
mariocstweb <mariocstmra@gmail.com>
2024-02-22 17:07:23 +0100

    added axios in script.js

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ $source_path = __DIR__ .'/../database/data.json';
 
 $tasks = file_get_contents($source_path);
 
-header('Content-Type: application/json');
+header('Content-Type: application/json', true);
 
 // echo json_encode($tasks);
 echo $tasks;
--- my_app/js/script.js
+++ my_app/js/script.js
@@ -1,5 +1,15 @@
+const endpoint = 'http://localhost/Boolean/php-todo-list-json/api/';
 const {createApp} = Vue;
 const app = createApp ({
+  name:"Todo List",
+  data: () =>({
+    tasks:[]
+  }),
+  created(){
+    axios.get(endpoint).then(res => {
+      this.tasks = res.data;
+    })
+  }
 
 })
 
