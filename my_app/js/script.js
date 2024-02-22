3530975a325822a7d082d5473f987d8cd7d9c8ea
mariocstweb <mariocstmra@gmail.com>
2024-02-22 18:13:49 +0100

    added data/axios/methods in script.js

--- my_app/js/script.js
+++ my_app/js/script.js
@@ -3,8 +3,20 @@ const {createApp} = Vue;
 const app = createApp ({
   name:"Todo List",
   data: () =>({
-    tasks:[]
+    tasks:[],
+    newTask: ''
   }),
+  methods:{
+    addTask(){
+      const data ={task: this.newTask}
+      const config ={headers: {'Content-Type' : 'multipart/form-data'}}
+      axios.post(endpoint, data, config).then(res =>{
+        this.tasks = res.data;
+        this.newTask = '';
+      })
+    }
+
+  },
   created(){
     axios.get(endpoint).then(res => {
       this.tasks = res.data;
