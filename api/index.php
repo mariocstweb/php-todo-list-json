aa12c31dbb7dcc9688dcaeccec8e3f34c6b4c56d
mariocstweb <mariocstmra@gmail.com>
2024-02-22 18:13:28 +0100

    added newTask in index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,32 @@
 <?php
-
-$source_path = __DIR__ .'/../database/data.json';
-
-$tasks = file_get_contents($source_path);
-
+// Converto in linguaggio Json
 header('Content-Type: application/json', true);
+// Path Json
+$source_path = __DIR__ .'/../database/data.json';
+// Recupero i contenuti json
+$data_content = file_get_contents($source_path);
 
-// echo json_encode($tasks);
+$tasks = $data_content;
+
+// Nuovo task tramite POST
+$new_task = $_POST['task'] ?? '';
+if($new_task) {
+  // Converto in php
+  $tasks = json_decode($tasks, true);
+  // Creo il nuovo oggetto
+  $new_task = array(
+    'id' => count($tasks) + 1,
+    'text' => $new_task,
+    'done' => false
+  );
+  // Pusho il nuovo task
+  $tasks[] = $new_task;
+  // Converto in json
+  $tasks = json_encode($tasks);
+
+  file_put_contents($source_path, $tasks);
+}
+
+
+// Task riconvertiti in json
 echo $tasks;
